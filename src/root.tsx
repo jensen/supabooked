2a1ed2f3f27b7d92e525725128c8d2358c294986
Karl Jensen <karljensen@gmail.com>
2022-08-20 10:08:03 -0700

    🚑️ Rename application

--- src/root.tsx
+++ src/root.tsx
@@ -18,7 +18,7 @@ import styles from "./styles/main.css";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Supabooked",
   viewport: "width=device-width,initial-scale=1",
 });
 
